ea958a7093a43592f28cabee70b524a64b0d7f9c
kathycrv14 <katherinecristinarv@outlook.com>
2023-01-15 22:20:43 -0500

    Mensaje de commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DirectivaModule } from './directiva/directiva.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    DirectivaModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/directiva/angular/angular.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-angular',
+  templateUrl: './angular.component.html',
+  styleUrls: ['./angular.component.css']
+})
+export class AngularComponent {
+  public inputCheckBox: boolean;
+
+  constructor() {this.inputCheckBox = false; }
+
+}
